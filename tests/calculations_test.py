4910a3f884e8d8e19e08a323139158493ce7061c
ybav1n <ybavn49@gmail.com>
2026-03-27 06:00:04 +0000

    Mona changes made4

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -74,14 +74,3 @@ def test_get_nth_fibonacci_negative():
     # Act & Assert
     with pytest.raises(ValueError):
         get_nth_fibonacci(n)
-
-# def test_get_nth_fibonacci_ten():
-#     """Test with n=10."""
-#     # Arrange
-#     n = 10
-
-#     # Act
-#     result = get_nth_fibonacci(n)
-
-#     # Assert
-#     assert result == 89
